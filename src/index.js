f9cf1c9aa3a2f0e8eee8700607f17cec508d6353
KhangNguyen <ntkhang2003@gmail.com>
2022-04-22 18:50:03 +0700

    Post method

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 //HTTP logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 //Template engine
 app.engine('.hbs', engine({
@@ -22,6 +26,15 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.render('news')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.send('')
 })
 
 app.listen(port, () => {
